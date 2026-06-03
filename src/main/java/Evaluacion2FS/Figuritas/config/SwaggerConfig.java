0d761e327cbcdf2e6a8a31b5ffcb6d9db1fabdca
Lucaaaastt <lucasplaza.nm@gmail.com>
2026-06-03 15:33:52 -0400

    Se agrego el config (dependencias y config listos )

---
+++ src/main/java/Evaluacion2FS/Figuritas/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package Evaluacion2FS.Figuritas.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    return new OPENAPI()
+    .info(new Info)()
+    .title
+
+}
