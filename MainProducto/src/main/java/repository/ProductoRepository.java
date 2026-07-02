71b8c8d840af85db080dc475f76dc455f42a9456
Lucaaaastt <lucasplaza.nm@gmail.com>
2026-07-01 22:45:13 -0400

    Se eliminan archivos clonados de MainProducto

--- MainProducto/src/main/java/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import model.Producto;
-
-// interfaz de acceso a datos para la tabla producto
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
